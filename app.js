fc93dd0922a1dc0993e3811cefe6253cceb34bc7
trieuquocdatb1910052 <datb1910052@student.ctu.edu.vn>
2023-02-16 16:06:54 +0700

    Cai dat xu li loi

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 
-const app = express();
+const ApiError = require("./app/api-error");
 
 const contactsRouter = require("./app/routes/contact.route");
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
@@ -13,5 +15,15 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
 
 module.exports = app;
